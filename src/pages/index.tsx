a00da7d95423c2137277d669ab68727c8a625d70
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-02-15 00:17:53 +0100

    feat: show mumbles (accesstoken is missing)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,47 @@
-import { GetServerSideProps, InferGetStaticPropsType } from 'next';
-
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { Header } from '../components/Header';
 import { ContentCard } from '../components/ContentCard';
 import { ContentInput } from '../components/ContentInput';
 
 import { Headline, Grid } from '@smartive-education/pizza-hawaii';
 
+import { fetchMumbles, fetchUsers, Mumble } from '../services/qwacker';
 import User from './../data/user.json';
-import { Post as PostType } from '../types/Post';
+import { useState } from 'react';
 
 type PageProps = {
-	posts: {
-		data: PostType[];
-	};
+	count: number;
+	mumbles: Mumble[];
+	error?: string;
 };
 
-export default function PageHome({ posts }: PageProps): InferGetStaticPropsType<typeof getServerSideProps> {
+export default function PageHome({
+	count,
+	mumbles: initialMumbles,
+	error,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	const [mumbles, setMumbles] = useState(initialMumbles);
+	const [loading, setLoading] = useState(false);
+	const [hasMore, setHasMore] = useState(initialMumbles.length < count);
+
+	if (error) {
+		return <div>An error occurred: {error}</div>;
+	}
+
+	const loadMore = async () => {
+		const { count, mumbles: newMumbles } = await fetchMumbles({
+			limit: 5,
+			offset: mumbles.length,
+		});
+
+		setLoading(false);
+		setHasMore(mumbles.length + newMumbles.length < count);
+		setMumbles([...mumbles, ...newMumbles]);
+	};
 	const user = {
 		...User,
 		profileLink: `user/${User.userName}`,
 	};
-
-	const Posts = posts.data;
 
 	return (
 		<div className="bg-slate-100">
@@ -46,18 +66,56 @@ export default function PageHome({ posts }: PageProps): InferGetStaticPropsType<
 							placeHolderText="Deine Meinung zählt"
 						/>
 
-						{Posts &&
-							Posts.sort((a: PostType, b: PostType) => {
-								return new Date(b.createdAt) > new Date(a.createdAt) ? 1 : -1;
-							}).map((post) => {
-								return <ContentCard key={post.id} variant="timeline" post={post} />;
-							})}
+						{mumbles.map((mumble) => {
+							return <ContentCard key={mumble.id} variant="timeline" post={mumble} />;
+						})}
 					</Grid>
+
+					{hasMore ? (
+						<button
+							onClick={() => loadMore()}
+							disabled={loading}
+							className="bg-indigo-400 px-2 py-1 rounded-lg mt-4"
+						>
+							{loading ? '...' : 'Load more'}
+						</button>
+					) : (
+						''
+					)}
 				</section>
 			</main>
 		</div>
 	);
 }
-export const getServerSideProps: GetServerSideProps = async () => ({
-	props: { posts: require('../data/posts.json') },
-});
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async () => {
+	//const { data: token } = useSession();
+	try {
+		const { count, mumbles } = await fetchMumbles({ limit: 5 });
+		const { users } = await fetchUsers({
+			accessToken: '',
+		});
+		console.log(users);
+		return {
+			props: {
+				count,
+				mumbles: mumbles.map((mumble) => {
+					const author = users.find((user) => user.id === mumble.creator);
+					if (author) {
+						mumble.creator = author;
+					}
+					return mumble;
+				}),
+			},
+		};
+	} catch (error) {
+		let message;
+		if (error instanceof Error) {
+			message = error.message;
+		} else {
+			message = String(error);
+		}
+
+		return { props: { error: message, mumbles: [], count: 0 } };
+	}
+};
