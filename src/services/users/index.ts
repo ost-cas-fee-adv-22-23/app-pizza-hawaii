3b92d4cc22894f1b74ec3748a672de533c116120
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-02-25 22:26:45 +0100

    feat: improve API service (load all - not yet perfect) and content card logic (handle likes, show count of replies and copy full url)

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -1,30 +1,66 @@
 import { TUser } from '../../types';
 
-type RawUser = Omit<TUser, 'createdAt profileLink'>;
+type TRawUser = Omit<TUser, 'createdAt profileLink'>;
 
-type TUserResponse = {
-	count: number;
-	data: RawUser[];
+/**
+ * Get all users
+ *
+ * @param {number} limit
+ * @param {number} offset
+ * @param {string} accessToken
+ *
+ * @returns {Promise<{ count: number; users: TUser[] }>}
+ */
+
+type TGetUser = {
+	limit?: number;
+	offset?: number;
+	accessToken: string;
 };
-export type TUploadImage = File & { preview: string };
 
-const getUsers = async (params?: { limit?: number; offset?: number; accessToken: string }) => {
-	const { limit, offset, accessToken } = params || {};
+type TGetUserResult = {
+	count: number;
+	users: TUser[];
+};
 
-	const url = `${process.env.NEXT_PUBLIC_QWACKER_API_URL}users?${new URLSearchParams({
-		limit: limit?.toString() || '10',
-		offset: offset?.toString() || '0',
-	})}`;
+const getUsers = async ({ limit, offset = 0, accessToken }: TGetUser): Promise<TGetUserResult> => {
+	const maxLimit = 1000;
+
+	// create url params
+	const urlParams = new URLSearchParams({
+		offset: offset.toString(),
+	});
+
+	if (limit !== undefined) {
+		urlParams.set('limit', Math.min(limit, maxLimit).toString());
+	}
+
+	const url = `${process.env.NEXT_PUBLIC_QWACKER_API_URL}users?${urlParams}`;
 
 	const res = await fetch(url, {
+		method: 'GET',
 		headers: {
 			'content-type': 'application/json',
 			Authorization: `Bearer ${accessToken}`,
 		},
 	});
-	const { count, data } = (await res.json()) as TUserResponse;
 
-	const users = data.map(transformUser);
+	const { count, data } = (await res.json()) as { count: number; data: TRawUser[] };
+	const users = data.map(transformUser) as TUser[];
+
+	// load more users if limit is not set and the amount of users is less than the total count
+	if (limit === undefined && users.length < count) {
+		const remainingOffset = offset + users.length;
+		const { users: remainingUsers } = await getUsers({
+			offset: remainingOffset,
+			accessToken,
+		});
+
+		return {
+			count,
+			users: [...users, ...remainingUsers],
+		};
+	}
 
 	return {
 		count,
@@ -32,10 +68,24 @@ const getUsers = async (params?: { limit?: number; offset?: number; accessToken:
 	};
 };
 
-const getUserById = async (params?: { id: string; accessToken: string }) => {
-	const { id, accessToken } = params || {};
-	console.log(params);
+/**
+ * Get a single user by id
+ *
+ * @param {string} id
+ * @param {string} accessToken
+ *
+ * @returns {Promise<TUser>}
+ *
+ * @throws {Error} if no valid id was provided
+ * @throws {Error} if the response was not ok
+ *
+ */
+type TGetUserById = {
+	id: string;
+	accessToken: string;
+};
 
+const getUserById = async ({ id, accessToken }: TGetUserById) => {
 	const url = `${process.env.NEXT_PUBLIC_QWACKER_API_URL}users/${id}`;
 
 	const res = await fetch(url, {
@@ -44,12 +94,29 @@ const getUserById = async (params?: { id: string; accessToken: string }) => {
 			Authorization: `Bearer ${accessToken}`,
 		},
 	});
-	const user = (await res.json()) as RawUser;
+	const user = (await res.json()) as TRawUser;
 	return transformUser(user);
 };
 
-// get User of a given Post Id
-const getUserbyPostId = async (id: string, accessToken?: string) => {
+/**
+ * Get a single user by id
+ *
+ * @param {string} id
+ * @param {string} accessToken
+ *
+ * @returns {Promise<TUser>}
+ *
+ * @throws {Error} if no valid id was provided
+ * @throws {Error} if the response was not ok
+ * @throws {Error} if the response was unauthorized
+ */
+
+type TGetUserByPostId = {
+	id: string;
+	accessToken: string;
+};
+
+const getUserbyPostId = async ({ id, accessToken }: TGetUserByPostId) => {
 	if (!id) {
 		throw new Error('getUserByPostId: No valid UserId was provided');
 	}
@@ -70,14 +137,14 @@ const getUserbyPostId = async (id: string, accessToken?: string) => {
 			throw new Error('getUserByPostId: Something went wrong with the response!');
 		}
 
-		const user = (await res.json()) as RawUser;
+		const user = (await res.json()) as TRawUser;
 		return transformUser(user);
 	} catch (error) {
 		throw new Error('getUserByPostId: could not reach API');
 	}
 };
 
-const transformUser = (user: RawUser) => ({
+const transformUser = (user: TRawUser): TUser => ({
 	...user,
 	profileLink: `/user/${user.userName}`,
 	//createdAt: new Date(decodeTime(user.id)).toISOString(),
@@ -87,5 +154,4 @@ export const usersService = {
 	getUsers,
 	getUserById,
 	getUserbyPostId,
-	// hier kommen weitere Methoden für den Users-Service
 };
