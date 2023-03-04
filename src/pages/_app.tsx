97f5ea3795edb563a34746cb2d5c517f0fb41a29
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-03-05 00:06:20 +0100

    feat: improve tailwind setup (remove hawai css and use tailwind config instead)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+
 import '../styles/globals.css';
-import '@smartive-education/pizza-hawaii/dist/bundle.css';
 import '@fontsource/poppins/500.css';
 import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-	//presets: [require("@smartive-education/pizza-hawaii/tailwind")],
-	content: ['./app/**/*.{ts,tsx}', './pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+	presets: [require('@smartive-education/pizza-hawaii/tailwind')],
+	content: ['./src/**/*.{ts,tsx}', './node_modules/@smartive-education/pizza-hawaii/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
 	},
