e29868b607a9a3e2b2f3218b148a6e4396f5325f
Felix Adam <felix.adam@tx.group>
2023-03-13 23:34:37 +0100

    feat: ErrorBoundaries 2 version both nasty error

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import FourOhFourPage from '../pages/404';
+import GlobalError from '../pages/global-error';
+
+interface Props {
+	className?: string;
+}
+
+interface State {
+	error?: Error;
+	hasError: boolean;
+}
+
+// yes this is a class component, because we need to use componentDidCatch
+class ErrorBoundary extends React.Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+		this.state = { hasError: false };
+	}
+	static getDerivedStateFromError(error: unknown) {
+		return { hasError: true, error };
+	}
+
+	componentDidCatch(error: Error | unknown) {
+		// may we also introduce an error logging service here - if there is time for that
+		console.error({ error });
+		const message = error?.toString() || 'unknown';
+		return <FourOhFourPage error={Error} reason={message} />;
+	}
+
+	render() {
+		// Check if the error is thrown
+		if (this.state.hasError) {
+			return <GlobalError error={this.state.error} />;
+		} else {
+			return this.props.children;
+		}
+	}
+}
+
+export default ErrorBoundary;
--- src/pages/global-error.tsx
+++ src/pages/global-error.tsx
@@ -1,18 +1,21 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+/* eslint-disable @next/next/no-head-element */
 'use client';
 
-//global-error.js replaces the root layout.js when active and so must define its own <html> and <body> tags
-export default function GlobalError({ error, reset }: { error: Error; reset: () => void }) {
-	const { message, stack } = error;
-
+// global-error.js replaces the root layout.js when
+// active and so must define its own <html> and <body> tags
+export default function GlobalError({ error }: { error?: Error }) {
 	return (
 		<html>
 			<head>Mumble Global Error Page</head>
 			<body>
-				<h2>Something went wrong!</h2>
-				<p>Error Message: {message}</p>
-				<p>Error Stack: {stack}</p>
+				<h2>Something went really wrong!</h2>
+				<p>Try to screenshot this page and send it to your friendly printer-fixer admin</p>
+				<p>Error Message: {error?.message}</p>
+				<p>Error Stack: {error?.stack}</p>
+				<br /> ¯\_(ツ)_/¯
 				<br />
-				<button onClick={() => reset()}>Try again</button>
+				<a href="/">Go back home</a>
 			</body>
 		</html>
 	);
