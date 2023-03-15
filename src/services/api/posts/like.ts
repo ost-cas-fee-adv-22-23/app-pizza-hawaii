fa45dfce47fd5b045cf2b5fc58765c173b17ab84
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-03-15 23:06:15 +0100

    docs: add JSDoc for like functions

--- src/services/api/posts/like.ts
+++ src/services/api/posts/like.ts
@@ -2,11 +2,25 @@ type TGetPost = {
 	id: string;
 };
 
+/*
+ * Like a post
+ *
+ * @param {string} id - The id of the post to like
+ * @returns {Promise<Response>}
+ */
+
 export const like = async ({ id }: TGetPost) => {
 	return await fetch(`api/posts/${id}/like`, {
 		method: 'PUT',
 	});
 };
+
+/*
+ * UnLike a post
+ *
+ * @param {string} id - The id of the post to like
+ * @returns {Promise<Response>}
+ */
 
 export const unlike = async ({ id }: TGetPost) => {
 	return await fetch(`api/posts/${id}/like`, {
