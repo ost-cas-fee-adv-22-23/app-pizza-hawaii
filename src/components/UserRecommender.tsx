cff8ec4b0dcd1e94fa909da78032662d683aef43
Felix Adam <felix.adam@tx.group>
2023-03-28 22:21:07 +0200

    feat: Recommender now smoothly

--- src/components/UserRecommender.tsx
+++ src/components/UserRecommender.tsx
@@ -2,13 +2,11 @@ import React, { FC, useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 
 import { Grid, Headline } from '@smartive-education/pizza-hawaii';
+import PreloaderAnimationCard from './helpers/PreloaderAnimation';
 import { UserCard } from './UserCard';
 import { services } from '../services';
 
 import { TUser } from '../types';
-import RecommenderPreloader from './helpers/RecommenderPreloader';
-import PreloaderAnimation from './helpers/PreloaderAnimation';
-import PreloaderAnimationCard from './helpers/PreloaderAnimation';
 
 type TUserRecommender = {
 	currentUserId: string;
@@ -19,6 +17,7 @@ export const UserRecommender: FC<TUserRecommender> = ({ currentUserId }: TUserRe
 	const [recommendedUsers, setRecommendedUsers] = useState([] as TUser[]);
 	const { data: session } = useSession();
 	const accessToken = session?.accessToken;
+	const index = [1, 2, 3, 4, 5, 6];
 
 	useEffect(() => {
 		setTimeout(() => {
@@ -28,21 +27,13 @@ export const UserRecommender: FC<TUserRecommender> = ({ currentUserId }: TUserRe
 						currentUserId,
 					});
 					setRecommendedUsers(recommendedUsers);
+					setIsLoading(false);
 				};
 				fetchRecommendedUsers();
-				setIsLoading(false);
 			}
-		}, 2000);
+		}, 600);
 	}, [accessToken, currentUserId]);
 
-	console.log('isLoading', isLoading);
-
-	// randomize the order of the recommended users
-	// const randomizedRecommendedUsers = recommendedUsers.sort(() => Math.random() - 0.5);
-	const index = [1, 2, 3, 4, 5, 6];
-	// exclude current user (yourself) from recommended users
-	const filteredRecommendedUsers = recommendedUsers.filter((user) => user.id !== currentUserId).splice(0, 6);
-	// TODO fix the height of preloader
 	return (
 		<>
 			<Headline as="h2" level={3}>
@@ -52,8 +43,12 @@ export const UserRecommender: FC<TUserRecommender> = ({ currentUserId }: TUserRe
 				<div className="mb-8">
 					<div className="flex flex-row flex-wrap -m-2">
 						{isLoading
-							? index.map((i) => <PreloaderAnimationCard key={i} />)
-							: filteredRecommendedUsers.map((user) => (
+							? index.map((i) => (
+									<div key={i} className="flex-initial w-4/12 p-3">
+										<PreloaderAnimationCard key={i} />
+									</div>
+							  ))
+							: recommendedUsers.map((user) => (
 									<div key={user.id} className="flex-initial w-4/12 p-3">
 										<UserCard key={user.id} user={user} />
 									</div>
