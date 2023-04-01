ad2973290170258da0bae955cc05c2caf505892f
Felix Adam <felix.adam@tx.group>
2023-04-01 18:40:37 +0200

    feat: new version of hawaii-comp-lib for image

--- src/components/Fullscreen.tsx
+++ src/components/Fullscreen.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/forbid-component-props */
 import React, { Dispatch, FC } from 'react';
 import { Image, Modal, Label } from '@smartive-education/pizza-hawaii';
 import { TPost } from '../types/Post';
@@ -11,14 +10,14 @@ type TFullscreen = {
 
 const Fullscreen: FC<TFullscreen> = (props: TFullscreen) => {
 	const { post, toggleHandler } = props;
-	console.log(post);
+
 	const close = () => {
 		toggleHandler(false);
 	};
 
 	return (
 		<Modal title={`Posted by: ${post.user.displayName}`} isVisible={true} onClose={() => close()}>
-			<div className="w-max-10/12 h-auto content-center mx-content ">
+			<div className="w-max-10/12 h-auto content-center mx-content">
 				<Image src={post.mediaUrl} alt={`Mumble-Post by Mumbel User ${post.user.userName}`} />
 				<br />
 				<Label as="legend" size="M">
