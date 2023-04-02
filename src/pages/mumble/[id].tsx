40da9ca9c5080736a7090b61580bef919802af9d
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-04-02 17:35:15 +0200

    feat: added PR change requests and fix postDetail

--- src/pages/mumble/[id].tsx
+++ src/pages/mumble/[id].tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { useSession } from 'next-auth/react';
 import { getToken } from 'next-auth/jwt';
@@ -15,9 +15,11 @@ type TUserPage = {
 	post: TPost;
 };
 
-const DetailPage: FC<TUserPage> = ({ post }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const DetailPage: FC<TUserPage> = ({ post: initialPost }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
 	const { data: session } = useSession();
 	const router = useRouter();
+
+	const [post, setPost] = useState(initialPost);
 
 	const onAddReply = async (postData: TAddPostProps) => {
 		const newReply = await services.posts.createPost({
@@ -25,20 +27,29 @@ const DetailPage: FC<TUserPage> = ({ post }: InferGetServerSidePropsType<typeof 
 			accessToken: session?.accessToken as string,
 		});
 
-		post.replies = [newReply, ...post.replies];
+		setPost((post: TPost) => ({
+			...post,
+			replies: [...(post.replies as TPost[]), newReply],
+		}));
+
 		return newReply;
 	};
 
 	const onRemovePost = async (id: string) => {
-		const result = await services.api.posts.remove({ id });
+		const response = await services.api.posts.remove({ id });
 
-		if (result) {
-			if (post.id === id) {
-				// go back to overview page
-				router.push('/');
-			} else {
-				post.replies = post.replies.filter((reply: TPost) => reply.id !== id);
-			}
+		if (!response.ok) {
+			throw new Error('Failed to delete post');
+		}
+
+		if (post.id === id) {
+			// go back to overview page
+			router.push('/');
+		} else {
+			setPost((post: TPost) => ({
+				...post,
+				replies: post.replies?.filter((reply: TPost) => reply.id !== id) as TPost[],
+			}));
 		}
 	};
 
