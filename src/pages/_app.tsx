2172a53665b7b65465feadb04d5c5e29365f25a2
Felix <felix.adam@tamedia.ch>
2023-04-06 16:15:31 +0200

    feat: pwa: manifest skeleton

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,7 +18,11 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 		<>
 			<Head>
 				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+				/>
+				<link rel="manifest" href="/manifest.json" />
 				<link rel="icon" type="image/svg+xml" href="./favicon.svg" />
 				<link rel="icon" type="image/png" href="./favicon.png" />
 			</Head>
