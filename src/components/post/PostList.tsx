7ac8efc2aab667560ebeaed81585ebec83d80681
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-04-17 09:46:02 +0200

    feat: mumbel detail improvements

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -9,27 +9,27 @@ type TPostListProps = {
 	posts?: TPost[];
 	variant?: TPostItemProps['variant'];
 	noPostsMessage?: string;
-	loadingItems?: number;
+	showLoadingItems?: number;
 	onRemovePost?: (id: string) => void;
 	onAnswerPost?: (id: string) => void;
 };
 
 export const PostList: FC<TPostListProps> = ({
-	posts,
+	posts = [],
 	variant = 'timeline',
-	loadingItems = 3,
+	showLoadingItems = 3,
 	noPostsMessage = 'Keine Posts vorhanden.',
 	onRemovePost,
 	onAnswerPost,
 }) => {
-	if (!posts?.length && loadingItems === 0) {
+	if (noPostsMessage && posts?.length === 0) {
 		return <p>{noPostsMessage}</p>;
 	}
-
+	// if there are no posts yet, show skeletons
 	return (
 		<Grid variant="col" gap="M" marginBelow="M">
-			{loadingItems > 0 && !posts?.length
-				? Array.from(Array(loadingItems).keys()).map((i) => <PostSkeleton key={i} showImage={i % 2 === 1} />)
+			{!posts && showLoadingItems > 0
+				? Array.from(Array(showLoadingItems).keys()).map((i) => <PostSkeleton key={i} showImage={i % 2 === 1} />)
 				: posts?.map((post: TPost) => {
 						return (
 							<PostItem
