2c5f3237088210b410f23f8b3547fb0aab0f66de
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-04-18 10:39:11 +0200

    feat: optimize login layout

--- src/components/layoutComponents/LoginLayout.tsx
+++ src/components/layoutComponents/LoginLayout.tsx
@@ -32,24 +32,25 @@ export const LoginLayout: FC<TLoginLayout> = ({ title, seo, children, header }) 
 				<meta property="og:site_name" content="Mumble" />
 				<meta property="og:url" content={currentUrl} />
 			</Head>
-			<div className="SplitScreen grid grid-rows-1 grid-cols-2 md:grid-rows-2 md:grid-cols-1 w-screen min-h-screen md:h-auto">
-				<header className="column-start-1 column-span-1 row-start-1 row-span-1 flex items-center justify-center bg-gradient-to-tl from-violet-600 to-pink-500">
+			<div className="SplitScreen grid grid-rows-1 grid-cols-2 md:grid-rows-[minmax(200px,_50vh)_auto] md:grid-cols-1 w-screen min-h-screen">
+				<header className="column-start-1 column-span-1 row-start-1 row-span-1 flex items-center justify-center bg-gradient-to-tl from-violet-600 to-pink-500 md:py-12">
 					{header ? (
 						header
 					) : (
 						<div className="w-8/12 text-pink-300 text-center">
-							<div className="inline-block mb-8">
+							<div className="inline-block my-8">
 								<NextImage src={VerticalLogo} alt="welcome to Mumble" />
 							</div>
-
-							<Headline level={1} as="h1">
-								Willkommen bei Mumble
-							</Headline>
+							<div className="md:hidden">
+								<Headline level={1} as="h1">
+									Willkommen bei Mumble
+								</Headline>
+							</div>
 						</div>
 					)}
 				</header>
 				<div className="column-start-2 column-span-1 row-start-1 row-span-1 md:column-start-1 md:row-start-2 flex flex-col items-center justify-center">
-					<main className="w-8/12 flex-auto flex flex-col items-center justify-center">
+					<main className="w-8/12 flex-auto flex flex-col items-center justify-center md:py-20">
 						<section className="w-full">{children}</section>
 					</main>
 					<Footer />
