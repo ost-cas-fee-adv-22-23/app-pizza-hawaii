737ee4fc1263d29c5aaa6473ce2c72de879668ca
Felix Adam <felix.adam@tx.group>
2023-04-21 22:23:36 +0200

    fix: no meta in _document as forbidden and duplicate

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,8 +11,6 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
-					<meta charSet="utf-8" />
-					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 					<script
 						dangerouslySetInnerHTML={{
 							__html: `
