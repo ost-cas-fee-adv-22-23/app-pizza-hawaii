c7d46c70638046a2bf9fa2bb13166d5a230063e7
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-04-22 10:49:12 +0200

    feat: optimize error 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import { Headline, Link, Richtext } from '@smartive-education/pizza-hawaii';
+import { Grid, Headline, Label, Link, Richtext } from '@smartive-education/pizza-hawaii';
 import NextLink from 'next/link';
 import React from 'react';
 
@@ -8,24 +8,28 @@ import { LoginLayout } from '../components/layoutComponents/LoginLayout';
 const FourOhFourPage = () => {
 	return (
 		<LoginLayout title="Mumble - 404">
-			<>
-				<Headline level={1} as="h1">
-					<span className="text-pink-600">Upsiii...</span>
-				</Headline>
-				<Headline level={2} as="h3">
-					<span className="text-violet-600">HTTP 404 - Page not Found</span>
+			<Headline level={1} as="p">
+				<span className="text-pink-600">Upsiii...</span>
+			</Headline>
+			<Headline level={2} as="h1">
+				<span className="text-violet-600">HTTP 404 - Page not Found</span>
+			</Headline>
+			<br />
+			<Label as="p" size="XL">
+				Something went south ...
+			</Label>
+			<br />
+			<Richtext size="M" as="div">
+				<p>
+					We are sorry, but the page you are looking for does not exist.
 					<br />
-					Something went south on ClientSide..
-				</Headline>
-				<br />
-				<Richtext size="M" as="div">
-					ErrorMessage: - Page not Found
-				</Richtext>
-				<br />
-				<Link href="/" component={NextLink}>
-					Back Home
-				</Link>
-			</>
+					If you think this is a bug, please contact the administrator of this site.
+				</p>
+			</Richtext>
+			<br />
+			<Link href="/" component={NextLink}>
+				Back Home
+			</Link>
 		</LoginLayout>
 	);
 };
