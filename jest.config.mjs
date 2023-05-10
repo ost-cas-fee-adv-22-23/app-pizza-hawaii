2bdf25b32b1180168e07960a7f2142dc8dd7997d
Felix <felix.adam@tamedia.ch>
2023-05-11 00:20:51 +0200

    feat: configure jest lib and RTL initial footer component tests working

---
+++ jest.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+import nextJest from 'next/jest.js';
+
+const babelConfigStyledComponents = {
+	presets: [['next/babel', { 'preset-react': { runtime: 'automatic' } }]],
+};
+
+const customJestConfig = {
+	verbose: true,
+	testEnvironment: 'jest-environment-jsdom',
+	moduleNameMapper: {
+		'@/(.*)$': '<rootDir>/src/$1',
+		'@smartive-education/pizza-hawaii': '<rootDir>/node_modules/@smartive-education/pizza-hawaii/dist/index.js',
+		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/src/__mocks__/fileMock.js',
+	},
+	/* Use babel-jest to transpile tests with the next/babel preset
+    https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+	transform: {
+		'^.+\\.(js|jsx|ts|tsx|mjs)$': ['babel-jest', babelConfigStyledComponents],
+	},
+	testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+	// An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+	testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/, <rootDir>/src/__tests__/__mocks__/'],
+};
+
+const createJestConfig = nextJest({ dir: './' });
+
+const config = async () => ({
+	...(await createJestConfig(customJestConfig)()),
+	transformIgnorePatterns: ['node_modules/(?!(@smartive-education/pizza-hawaii)/)', '^.+\\.module\\.(css|sass|scss)$'],
+});
+
+export default config;
---
+++ src/components/Footer.test.tsx
@@ -0,0 +1,24 @@
+// test render footer
+import { cleanup, render, screen } from '@testing-library/react';
+
+import { Footer } from './Footer';
+
+describe('Footer Component', () => {
+	afterEach(cleanup);
+	it('should render both great Authors in the footer text', () => {
+		render(<Footer />);
+		expect(screen.getByText('Felix Adam' && 'Jürgen Rudigier'));
+		expect(screen.queryByLabelText('Toggle theme'));
+	});
+
+	it('should have a toggle theme button with the label `Toggle theme`', () => {
+		render(<Footer />);
+		expect(screen.getByRole('button', { name: 'Toggle theme' }));
+		expect(screen.queryByLabelText('Toggle theme'));
+	});
+
+	it('should have a HTML element footer and the className of `Footer` ', () => {
+		const { container } = render(<Footer />);
+		expect(container.querySelector('footer'));
+	});
+});
