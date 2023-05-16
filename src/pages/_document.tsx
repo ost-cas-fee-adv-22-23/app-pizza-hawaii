dcf32579504cfd54182d7dbc8bc601ce34533c06
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-05-16 21:34:53 +0200

    feat: remove unneccesary function in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,7 @@
-import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
-	}
-
 	render() {
 		return (
 			<Html lang="en">
