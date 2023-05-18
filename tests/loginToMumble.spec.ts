1c71f0545989b8762ab8fb94a185523db45d7a7c
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-05-18 23:04:36 +0200

    feat: improve e2e test for mumbels

--- tests/loginToMumble.spec.ts
+++ tests/loginToMumble.spec.ts
@@ -1,38 +1,51 @@
 import { expect, test } from '@playwright/test';
-
 test.describe('Login to mumble', () => {
-	test('login to Mumble timeline ', async ({ page }) => {
-		await page.goto(process.env.NEXT_PUBLIC_URL as string);
+	const timelineUrl = process.env.NEXT_PUBLIC_VERCEL_URL as string;
+	const timelineTitle = 'Mumble - Welcome to Mumble';
+
+	test('Login to Mumble ', async ({ page }) => {
+		// generate a random text for the post
+		const exampleText = `Pizza Hawaii Test #pht #${Math.random().toString(36).substring(7)} (${new Date().getTime()})`;
+
+		// open the login page
+		await page.goto(timelineUrl);
 		await page.getByRole('button', { name: 'Login via Zitadel' }).click();
-		await expect(page).toHaveURL(/.*.zitadel.cloud/);
+
+		// check if we are on the zitadel login page
+		await expect(page).toHaveURL(/.*.zitadel.cloud\/ui\/login\/login.*/);
+
+		// fill in the username
 		await page.fill('input[name="loginName"]', process.env.ZITADEL_USERNAME as string);
 		const forwardBtn = page.getByText('next');
 		await forwardBtn.click();
+
+		// fill in the password
 		await page.fill('input[name="password"]', process.env.ZITADEL_PASSWORD as string);
 		const forwardBtnLogin = page.getByText('next');
 		await forwardBtnLogin.click();
+
 		// redirect to mumble timeline
-		await expect(page).toHaveURL('http://localhost:3000/');
-		await expect(page).toHaveTitle('Mumble - Welcome to Mumble');
+		await expect(page).toHaveURL(timelineUrl);
+		await expect(page).toHaveTitle(timelineTitle);
 
 		// write a mumble text and post it
-		const postTextArea = page.getByPlaceholder('Deine Meinung');
-		await postTextArea.fill('Hello World - what a nice day to write End to End Tests!');
-		const postBtn = page.getByRole('button', { name: 'Absenden' });
+		const postTextArea = page.getByPlaceholder('Deine Meinung zählt');
+		await expect(postTextArea).toBeVisible();
+		await postTextArea.fill(exampleText);
+		const postBtn = page.getByText('Absenden');
 		await postBtn.click();
 
 		// check if the post is in the timeline
 		// TODO would be nice to refresh on the button click... but its appearence is too slow
 		// await page.getByRole('button', { name: 'World is changing, update your feed.' }).waitFor();
-		await page.goto(process.env.NEXT_PUBLIC_URL as string);
 
-		await expect(page.getByText('what a nice day to write End to End Tests!')).toBeVisible();
+		await expect(page.getByText(exampleText)).toBeVisible();
 
 		// now delete that post again
 		const deleteBtn = page.getByRole('button', { name: 'Delete' });
 		await deleteBtn.click();
-		await expect(page).toHaveURL('http://localhost:3000/');
-		await expect(page).toHaveTitle('Mumble - Welcome to Mumble');
-		await expect(page.getByText('what a nice day to write End to End Tests!')).not.toBeVisible();
+
+		// check if the post is gone from the timeline
+		await expect(page.getByText(exampleText)).not.toBeVisible();
 	});
 });
