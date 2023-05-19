7210eb2864ae624a80223fb0933aa647a796023f
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-05-19 23:42:49 +0200

    test: improve test

--- tests/registerForm.spec.ts
+++ tests/registerForm.spec.ts
@@ -1,9 +1,40 @@
-import { expect, test } from '@playwright/test';
+import { expect, Page, test } from '@playwright/test';
 import { describe } from 'node:test';
 
 const appUrl = process.env.NEXT_PUBLIC_VERCEL_URL as string;
 const signupUrl = `${appUrl}/auth/signup`;
 const signupPageTitle = 'Mumble - Registrierung';
+const passwordMismatchFeedback = 'Die Passwörter stimmen nicht überein.';
+
+const getPasswordFields = async (page: Page) => {
+	// get form fields
+	const passwordField = page.locator('input[name="password"]');
+
+	const confirmPasswordField = page.locator('input[name="confirmPassword"]');
+	await expect(passwordField).toBeVisible();
+	await expect(confirmPasswordField).toBeVisible();
+
+	console.log('passwordField', await passwordField);
+	console.log('confirmPasswordField', await confirmPasswordField);
+
+	// get aria-describedby linked text
+	const confirmPasswordFieldAriaDescribedBy = await confirmPasswordField.getAttribute('aria-describedby');
+
+	console.log('confirmPasswordFieldAriaDescribedBy', confirmPasswordFieldAriaDescribedBy);
+
+	// get the user feedback element
+	const confirmPasswordFieldUserFeedback = confirmPasswordFieldAriaDescribedBy
+		? page.locator(`#${confirmPasswordFieldAriaDescribedBy}`)
+		: null;
+
+	console.log('confirmPasswordFieldUserFeedback', confirmPasswordFieldUserFeedback);
+
+	return {
+		passwordField,
+		confirmPasswordField,
+		confirmPasswordFieldUserFeedback,
+	};
+};
 
 describe('Register Form works: if two different password strings are provided, there is a userFeedback and Submit Button is disabled', () => {
 	test('Register Form is visible', async ({ page }) => {
@@ -13,27 +44,39 @@ describe('Register Form works: if two different password strings are provided, t
 		await expect(page).toHaveTitle(signupPageTitle);
 		// check if userName field is visible
 		await expect(page.getByLabel('Username')).toBeVisible();
-		// provide different passwords in password and repeat password field
-		const passwordField = page.locator('input[name="password"]');
-		const passwordRepeatField = page.locator('input[name="passwordRepeat"]');
-		passwordField.fill('secretPa$$word');
-		passwordRepeatField.fill('secretPa$$word-2different');
+	});
+
+	test('Register Form is working when passwords are the different', async ({ page }) => {
+		// load the register page
+		await page.goto(signupUrl);
+
+		// fill both password fields with different passwords
+		await page.fill('input[name="password"]', 'secretPa$$word');
+		await page.fill('input[name="confirmPassword"]', 'secretPa$$word-2different');
+
 		// wait 100ms
 		await page.waitForTimeout(100);
-		expect(passwordRepeatField).toHaveAttribute('user-message', 'Die Passwörter stimmen nicht überein.');
-		expect(page.getByRole('button', { name: 'Speichern' })).toBeDisabled();
+
+		const passwordMismatchFeedbackEl = await page.getByText(passwordMismatchFeedback);
+
+		// check if the user feedback element is visible
+		await expect(passwordMismatchFeedbackEl).toBeVisible();
 	});
 
 	test('Register Form is working when passwords are the same', async ({ page }) => {
 		// load the register page
 		await page.goto(signupUrl);
+
 		// fill both password fields with the same password
-		const passwordField = page.locator('input[name="password"]');
-		const passwordRepeatField = page.locator('input[name="passwordRepeat"]');
-		passwordRepeatField.fill('secretPa$$word');
-		passwordField.fill('secretPa$$word');
+		await page.fill('input[name="password"]', 'secretPa$$word');
+		await page.fill('input[name="confirmPassword"]', 'secretPa$$word');
+
+		// wait 100ms
 		await page.waitForTimeout(100);
-		// check if the submit button is enabled
-		expect(page.getByRole('button', { name: 'Speichern' })).toBeEnabled();
+
+		const passwordMismatchFeedbackEl = await page.getByText(passwordMismatchFeedback);
+
+		// check if the user feedback element is not visible
+		await expect(passwordMismatchFeedbackEl).not.toBeVisible();
 	});
 });
