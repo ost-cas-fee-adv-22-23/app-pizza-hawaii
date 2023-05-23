f8453b2f38e1ddd03aa8c15d46c7ffdc48b56035
Felix <felix.adam@tamedia.ch>
2023-05-23 15:48:06 +0200

    feat: next try localhost 3000

--- lighthouse.js
+++ lighthouse.js
@@ -1,5 +1,10 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'npm run start',
+      numberOfRuns: 3,
+    },
     upload: {
       target: 'temporary-public-storage',
     },
