8018f782303e024ebf3224bba2a8790ab7009851
Felix Adam <felix.adam@tx.group>
2023-05-25 17:04:26 +0200

    feat: with node 18

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,10 +3,12 @@ const preset = process.env.DESKTOP ? 'desktop' : undefined
 module.exports = {
   ci: {
     collect: {
+      startServerCommand: 'npm run build && npm run start',
+      startServerReadyPattern: 'ready on',
       url: ['https://app-pizza-hawaii.vercel.app/auth/login',
             'https://app-pizza-hawaii.vercel.app/auth/signup'
       ],
-      numberOfRuns: 3,
+      numberOfRuns: 1,
       settings: { preset },
     },
     upload: {
