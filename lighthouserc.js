313402e9fd1e0730fbd80f89a6c1f7a45937a341
Felix <felix.adam@tamedia.ch>
2023-05-31 20:02:31 +0200

    feat: lighthouse assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -10,8 +10,19 @@ module.exports = {
       url: [`${LHCI_SERVER_BASE_URL}/auth/login`,
             `${LHCI_SERVER_BASE_URL}/auth/signup`
       ],
-      numberOfRuns: 1,
+      numberOfRuns: 3,
       settings: { preset: 'desktop'},
+    },
+    assert: {
+      "assertions": {
+        "first-contentful-paint": ["warn", {"maxNumericValue": 3000}],
+        "categories:performance": ["warn", {"minScore": 0.9}],
+        "categories:accessibility": ["warn", {"minScore": 0.9}],
+        "dom-size": ["error", {"maxNumericValue": 3000}],
+        "categories:seo": ["error", {"minScore": 0.9}],
+        "installable-manifest": "on",
+        "service-worker": "on",
+      },
     },
     upload: {
       target: 'temporary-public-storage',
