3c52d762def87c17f79d657b5d4bd709e8508a69
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-06-01 23:00:07 +0200

    feat: improve login flow

--- tests/loginToMumble.spec.ts
+++ tests/loginToMumble.spec.ts
@@ -3,35 +3,45 @@ import { expect, test } from '@playwright/test';
 test.describe('Login to Application, create a MumblePost, test its appearence and delete this post.', () => {
 	const timelineUrl = process.env.NEXT_PUBLIC_VERCEL_URL as string;
 	const timelineTitle = 'Mumble - Welcome to Mumble';
-	const logoutUrl = `${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/login`;
+	const loginUrl = `${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/login`;
 
 	test('Login to Mumble ', async ({ page }) => {
-		// generate a random text for the post
-		const exampleText = `Pizza Hawaii Test #pht #${Math.random().toString(36).substring(7)} (${new Date().getTime()})`;
+		// Generate a random text for the post
+		const exampleText = `Pizza Hawaii Test #pht #${Math.random().toString(36).substring(7)}`;
 
-		// open the login page
+		// Step 1: Open the login page via the Mumble login button
 		await page.goto(timelineUrl);
 		await page.getByRole('button', { name: 'Login via Zitadel' }).click();
 
-		// check if we are on the zitadel login page
+		// Check if we are on the zitadel login page
 		await expect(page).toHaveURL(/.*.zitadel.cloud\/ui\/login\/login.*/);
 
-		// fill in the username
+		// Step 2: Fill in the username
 		await page.fill('input[name="loginName"]', process.env.ZITADEL_USERNAME as string);
+		await page.click('input[name="loginName"]');
 		await page.keyboard.press('Enter');
 
-		// check if we are on the zitadel password page
+		const loginName = page.locator('input[name="loginName"]');
+		await expect(loginName).toBeVisible();
+		await loginName.click();
+		loginName.fill(process.env.ZITADEL_PASSWORD as string);
+		await page.keyboard.press('Enter');
+
+		// Check if we are on the zitadel password page
 		await expect(page).toHaveURL(/.*.zitadel.cloud\/ui\/login\/loginname.*/);
 
-		// fill in the password
-		await page.fill('input[name="password"]', process.env.ZITADEL_PASSWORD as string);
+		// Step 3: Fill in the password
+		const passwordField = page.locator('input[name="password"]');
+		await expect(passwordField).toBeVisible();
+		await passwordField.click();
+		passwordField.fill(process.env.ZITADEL_PASSWORD as string);
 		await page.keyboard.press('Enter');
 
-		// redirect to mumble timeline
+		// Check if we are redirected to mumble timeline
 		await expect(page).toHaveURL(timelineUrl);
 		await expect(page).toHaveTitle(timelineTitle);
 
-		// write a mumble text and post it
+		// Step 4: Write a mumble text and post it
 		const postTextArea = page.getByPlaceholder('Deine Meinung zählt');
 		await expect(postTextArea).toBeVisible();
 		await postTextArea.fill(exampleText);
@@ -40,20 +50,22 @@ test.describe('Login to Application, create a MumblePost, test its appearence an
 		const postText = page.getByText(exampleText, { exact: true });
 		await expect(postText).toBeVisible();
 
-		// get element with class 'PostItem' that contains the text
+		// Step 5: Get element with class 'PostItem' that contains the text
 		const postItem = page.locator(`.PostItem:has-text("${exampleText}")`);
 		await expect(postItem).toBeVisible();
 
-		// now delete the exact PostItem again
+		// Step 6: Delete the exact PostItem again
 		const deleteBtn = postItem.getByText('Delete', { exact: true });
 		await deleteBtn.click();
 
-		// check if the post is gone from the timeline
+		// Check if the post is gone from the timeline
 		await expect(page.getByText(exampleText, { exact: true })).not.toBeVisible();
 
-		// logout from mumble
+		// Step 7: Logout from mumble
 		const logoutBtn = page.getByRole('button', { name: 'Log out' });
 		await logoutBtn.click();
-		await expect(page).toHaveURL(logoutUrl);
+
+		// Check if we are redirected to the login url
+		await expect(page).toHaveURL(loginUrl);
 	});
 });
