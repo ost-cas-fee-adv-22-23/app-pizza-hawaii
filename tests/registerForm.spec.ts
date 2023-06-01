b15752971492ca7657fcac1548c60dfbd476dc21
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-06-01 21:27:03 +0200

    feat: improve registerForm Test

--- tests/registerForm.spec.ts
+++ tests/registerForm.spec.ts
@@ -5,6 +5,8 @@ const appUrl = process.env.NEXT_PUBLIC_VERCEL_URL as string;
 const signupUrl = `${appUrl}/auth/signup`;
 const signupPageTitle = 'Mumble - Registrierung';
 const passwordMismatchFeedback = 'Die Passwörter stimmen nicht überein.';
+const thePassword = 'secretPa$$word';
+const wrongPassword = 'secretPa$$word-2different';
 
 const getPasswordFields = async (page: Page) => {
 	// get form fields
@@ -46,13 +48,13 @@ describe('Register Form works: if two different password strings are provided, t
 		await expect(page.getByLabel('Username')).toBeVisible();
 	});
 
-	test('Register Form is working when passwords are the different', async ({ page }) => {
+	test('Register Form is working when passwords are different and passwordMismatchFeedback appears', async ({ page }) => {
 		// load the register page
 		await page.goto(signupUrl);
 
 		// fill both password fields with different passwords
-		await page.fill('input[name="password"]', 'secretPa$$word');
-		await page.fill('input[name="confirmPassword"]', 'secretPa$$word-2different');
+		await page.fill('input[name="password"]', thePassword);
+		await page.fill('input[name="confirmPassword"]', wrongPassword);
 
 		// wait 100ms
 		await page.waitForTimeout(100);
@@ -68,8 +70,8 @@ describe('Register Form works: if two different password strings are provided, t
 		await page.goto(signupUrl);
 
 		// fill both password fields with the same password
-		await page.fill('input[name="password"]', 'secretPa$$word');
-		await page.fill('input[name="confirmPassword"]', 'secretPa$$word');
+		await page.fill('input[name="password"]', thePassword);
+		await page.fill('input[name="confirmPassword"]', thePassword);
 
 		// wait 100ms
 		await page.waitForTimeout(100);
