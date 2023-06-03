9acfc978e12d937f618414b0df19eae2e8f44aba
Felix <felix.adam@tamedia.ch>
2023-06-03 15:08:11 +0200

    feat: better lighthouse assertions config

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,6 +1,4 @@
-const preset = process.env.LHCI_DESKTOP ? 'desktop' : undefined
 const LHCI_SERVER_BASE_URL = 'https://app-pizza-hawaii.vercel.app'
-
 
 module.exports = {
   ci: {
@@ -10,14 +8,15 @@ module.exports = {
       url: [`${LHCI_SERVER_BASE_URL}/auth/login`,
             `${LHCI_SERVER_BASE_URL}/auth/signup`
       ],
-      numberOfRuns: 3,
+      numberOfRuns: 2,
       settings: { preset: 'desktop'},
     },
     assert: {
       "assertions": {
         "first-contentful-paint": ["warn", {"maxNumericValue": 3000}],
-        "categories:performance": ["warn", {"minScore": 0.9}],
-        "categories:accessibility": ["warn", {"minScore": 0.9}],
+        "categories:performance": ["warn", {"minScore": 0.9, "aggregationMethod": "optimistic"}],
+        "categories:accessibility": ["warn", {"minScore": 1, "aggregationMethod": "optimistic"}],
+        "categories:best-practices": ["warn", {"minScore": 0.9, "aggregationMethod": "optimistic"}],
         "dom-size": ["error", {"maxNumericValue": 3000}],
         "categories:seo": ["error", {"minScore": 0.9}],
         "installable-manifest": "on",
@@ -29,4 +28,3 @@ module.exports = {
     },
   }
 };
-console.log('i', module.exports.ci.collect.url)
