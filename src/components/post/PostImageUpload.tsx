e06291ecfb09a6e3e3461a99cdfaa115a86a522c
Felix <felix.adam@tamedia.ch>
2023-06-16 22:57:26 +0200

    fix: test whole workflow with changed app

--- src/components/post/PostImageUpload.tsx
+++ src/components/post/PostImageUpload.tsx
@@ -28,8 +28,7 @@ export const PostImageUpload: FC<TPostImageUpload> = ({ onNewFile }) => {
 			console.error('onError:', error);
 		},
 	});
-	// eslint-disable-next-line no-console
-	console.log('test new release docker!');
+
 	return (
 		<div className="p-2 h-48 cursor-pointer flex justify-center align-middle bg-slate-100 text-slate-600">
 			<div {...getRootProps({ className: 'dropzone' })}>
