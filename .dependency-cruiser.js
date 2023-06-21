cb000d2c05cafabd05cd914fdbc1862a04f81ed0
Felix <felix.adam@tamedia.ch>
2023-06-21 19:56:36 +0200

    fix: run prettier fix configs

--- .dependency-cruiser.js
+++ .dependency-cruiser.js
@@ -19,122 +19,115 @@ module.exports = {
       - api- routes: we don't want to check the api routes, as they ar running at compile time. 
       - _document.tsx: is a nextjs file, which is running at compile time.
     */
-    {
-      name: 'no-orphans',
-      comment:
-        "This is an orphan module - it's likely not used (anymore?). Either use it or " +
-        "remove it. If it's logical this module is an orphan (i.e. it's a config file), " +
-        "add an exception for it in your dependency-cruiser configuration. By default " +
-        "this rule does not scrutinize dot-files (e.g. .eslintrc.js), TypeScript declaration " +
-        "files (.d.ts), tsconfig.json and some of the babel and webpack configs.",
-      severity: 'warn',
-      from: {
-        orphan: true,
-        pathNot: [
-          '(^|/)\\.[^/]+\\.(js|cjs|mjs|ts|json)$', // dot files
-          '\\.d\\.ts$',                            // TypeScript declaration files
-          '(^|/)tsconfig\\.json$',                 // TypeScript config
-          '(^|/)(babel|webpack)\\.config\\.(js|cjs|mjs|ts|json)$', // other configs
-          'src/middleware.ts',
-          'src/pages/global-error.tsx',
-          'src/pages/api/*',
-          'src/pages/_document.tsx',
-          'src/__tests__/*',
-        ]
-      },
-      to: {},
-    },
-    {
-      name: 'no-deprecated-core',
-      comment:
-        'A module depends on a node core module that has been deprecated. Find an alternative - these are ' +
-        "bound to exist - node doesn't deprecate lightly.",
-      severity: 'warn',
-      from: {},
-      to: {
-        dependencyTypes: [
-          'core'
-        ],
-        path: [
-          '^(v8\/tools\/codemap)$',
-          '^(v8\/tools\/consarray)$',
-          '^(v8\/tools\/csvparser)$',
-          '^(v8\/tools\/logreader)$',
-          '^(v8\/tools\/profile_view)$',
-          '^(v8\/tools\/profile)$',
-          '^(v8\/tools\/SourceMap)$',
-          '^(v8\/tools\/splaytree)$',
-          '^(v8\/tools\/tickprocessor-driver)$',
-          '^(v8\/tools\/tickprocessor)$',
-          '^(node-inspect\/lib\/_inspect)$',
-          '^(node-inspect\/lib\/internal\/inspect_client)$',
-          '^(node-inspect\/lib\/internal\/inspect_repl)$',
-          '^(async_hooks)$',
-          '^(punycode)$',
-          '^(domain)$',
-          '^(constants)$',
-          '^(sys)$',
-          '^(_linklist)$',
-          '^(_stream_wrap)$'
-        ],
-      }
-    },
-    {
-      name: 'not-to-deprecated',
-      comment:
-        'This module uses a (version of an) npm module that has been deprecated. Either upgrade to a later ' +
-        'version of that module, or find an alternative. Deprecated modules are a security risk.',
-      severity: 'warn',
-      from: {},
-      to: {
-        dependencyTypes: [
-          'deprecated'
-        ]
-      }
-    },
-    {
-      name: 'no-non-package-json',
-      severity: 'error',
-      comment:
-        "This module depends on an npm package that isn't in the 'dependencies' section of your package.json. " +
-        "That's problematic as the package either (1) won't be available on live (2 - worse) will be " +
-        "available on live with an non-guaranteed version. Fix it by adding the package to the dependencies " +
-        "in your package.json.",
-      from: {},
-      to: {
-        dependencyTypes: [
-          'npm-no-pkg',
-          'npm-unknown'
-        ]
-      }
-    },
-    {
-      name: 'not-to-unresolvable',
-      comment:
-        "This module depends on a module that cannot be found ('resolved to disk'). If it's an npm " +
-        'module: add it to your package.json. In all other cases you likely already know what to do.',
-      severity: 'error',
-      from: {},
-      to: {
-        couldNotResolve: true
-      }
-    },
-    {
-      name: 'no-duplicate-dep-types',
-      comment:
-        "Likely this module depends on an external ('npm') package that occurs more than once " +
-        "in your package.json i.e. bot as a devDependencies and in dependencies. This will cause " +
-        "maintenance problems later on.",
-      severity: 'warn',
-      from: {},
-      to: {
-        moreThanOneDependencyType: true,
-        // as it's pretty common to have a type import be a type only import 
-        // _and_ (e.g.) a devDependency - don't consider type-only dependency
-        // types for this rule
-        dependencyTypesNot: ["type-only"]
-      }
-    },
+		{
+			name: 'no-orphans',
+			comment:
+				"This is an orphan module - it's likely not used (anymore?). Either use it or " +
+				"remove it. If it's logical this module is an orphan (i.e. it's a config file), " +
+				'add an exception for it in your dependency-cruiser configuration. By default ' +
+				'this rule does not scrutinize dot-files (e.g. .eslintrc.js), TypeScript declaration ' +
+				'files (.d.ts), tsconfig.json and some of the babel and webpack configs.',
+			severity: 'warn',
+			from: {
+				orphan: true,
+				pathNot: [
+					'(^|/)\\.[^/]+\\.(js|cjs|mjs|ts|json)$', // dot files
+					'\\.d\\.ts$', // TypeScript declaration files
+					'(^|/)tsconfig\\.json$', // TypeScript config
+					'(^|/)(babel|webpack)\\.config\\.(js|cjs|mjs|ts|json)$', // other configs
+					'src/middleware.ts',
+					'src/pages/global-error.tsx',
+					'src/pages/api/*',
+					'src/pages/_document.tsx',
+					'src/__tests__/*',
+				],
+			},
+			to: {},
+		},
+		{
+			name: 'no-deprecated-core',
+			comment:
+				'A module depends on a node core module that has been deprecated. Find an alternative - these are ' +
+				"bound to exist - node doesn't deprecate lightly.",
+			severity: 'warn',
+			from: {},
+			to: {
+				dependencyTypes: ['core'],
+				path: [
+					'^(v8/tools/codemap)$',
+					'^(v8/tools/consarray)$',
+					'^(v8/tools/csvparser)$',
+					'^(v8/tools/logreader)$',
+					'^(v8/tools/profile_view)$',
+					'^(v8/tools/profile)$',
+					'^(v8/tools/SourceMap)$',
+					'^(v8/tools/splaytree)$',
+					'^(v8/tools/tickprocessor-driver)$',
+					'^(v8/tools/tickprocessor)$',
+					'^(node-inspect/lib/_inspect)$',
+					'^(node-inspect/lib/internal/inspect_client)$',
+					'^(node-inspect/lib/internal/inspect_repl)$',
+					'^(async_hooks)$',
+					'^(punycode)$',
+					'^(domain)$',
+					'^(constants)$',
+					'^(sys)$',
+					'^(_linklist)$',
+					'^(_stream_wrap)$',
+				],
+			},
+		},
+		{
+			name: 'not-to-deprecated',
+			comment:
+				'This module uses a (version of an) npm module that has been deprecated. Either upgrade to a later ' +
+				'version of that module, or find an alternative. Deprecated modules are a security risk.',
+			severity: 'warn',
+			from: {},
+			to: {
+				dependencyTypes: ['deprecated'],
+			},
+		},
+		{
+			name: 'no-non-package-json',
+			severity: 'error',
+			comment:
+				"This module depends on an npm package that isn't in the 'dependencies' section of your package.json. " +
+				"That's problematic as the package either (1) won't be available on live (2 - worse) will be " +
+				'available on live with an non-guaranteed version. Fix it by adding the package to the dependencies ' +
+				'in your package.json.',
+			from: {},
+			to: {
+				dependencyTypes: ['npm-no-pkg', 'npm-unknown'],
+			},
+		},
+		{
+			name: 'not-to-unresolvable',
+			comment:
+				"This module depends on a module that cannot be found ('resolved to disk'). If it's an npm " +
+				'module: add it to your package.json. In all other cases you likely already know what to do.',
+			severity: 'error',
+			from: {},
+			to: {
+				couldNotResolve: true,
+			},
+		},
+		{
+			name: 'no-duplicate-dep-types',
+			comment:
+				"Likely this module depends on an external ('npm') package that occurs more than once " +
+				'in your package.json i.e. bot as a devDependencies and in dependencies. This will cause ' +
+				'maintenance problems later on.',
+			severity: 'warn',
+			from: {},
+			to: {
+				moreThanOneDependencyType: true,
+				// as it's pretty common to have a type import be a type only import
+				// _and_ (e.g.) a devDependency - don't consider type-only dependency
+				// types for this rule
+				dependencyTypesNot: ['type-only'],
+			},
+		},
 
 		/* rules you might want to tweak for your specific situation: */
 		{
--- src/__tests__/components/PostCollection.test.tsx
+++ src/__tests__/components/PostCollection.test.tsx
@@ -2,133 +2,143 @@ import { cleanup, render, screen } from '@testing-library/react';
 import { PostCollection } from '../../components/post/PostCollection';
 import { useSession } from 'next-auth/react';
 import '@/__mocks__/routerMock';
-import  mockLoadedPosts from '@/__mocks__/loadedPosts.json';
-import { TPost } from '../../types/Post'
+import mockLoadedPosts from '@/__mocks__/loadedPosts.json';
+import { TPost } from '../../types/Post';
 import { PostList } from '@/components/post/PostList';
 
 jest.mock('next-auth/react');
 describe('PostCollection Component input rendering', () => {
-    
-    const loadedPosts = mockLoadedPosts as TPost[];
-        
-    afterAll(cleanup);
-    // test if PostCollection component renders correctly
-    it('should render the PostCollection component with text', () => {
-        const mockSession = {
+	const loadedPosts = mockLoadedPosts as TPost[];
+
+	afterAll(cleanup);
+	// test if PostCollection component renders correctly
+	it('should render the PostCollection component with text', () => {
+		const mockSession = {
 			user: { name: 'Filiks Adamski', email: 'filiks.adamski@mumble.com' },
 			expires: '1234567890',
 		};
 		(useSession as jest.Mock).mockReturnValue([mockSession, true]);
 
-        render(<PostCollection 
-            headline="Whats new in Mumble...."
-            posts={loadedPosts}
-            canLoadMore={true}
-            canAdd={true}
-            autoUpdate={true}
-         />);
-        expect(screen.getByText('Deine Meinung zählt'));
-    });
+		render(
+			<PostCollection
+				headline="Whats new in Mumble...."
+				posts={loadedPosts}
+				canLoadMore={true}
+				canAdd={true}
+				autoUpdate={true}
+			/>
+		);
+		expect(screen.getByText('Deine Meinung zählt'));
+	});
 
-    // test if PostCollection component renders correctly
-    it('should render the the load more Button if `canLoadMore` is true', () => {
-        render(<PostCollection
-            headline="Whats new in Mumble...."
-            posts={loadedPosts}
-            canLoadMore={true}
-            canAdd={true}
-            autoUpdate={true}
-         />);
-        expect(screen.getByRole('button', { name: 'Load more'}) );
-    });
+	// test if PostCollection component renders correctly
+	it('should render the the load more Button if `canLoadMore` is true', () => {
+		render(
+			<PostCollection
+				headline="Whats new in Mumble...."
+				posts={loadedPosts}
+				canLoadMore={true}
+				canAdd={true}
+				autoUpdate={true}
+			/>
+		);
+		expect(screen.getByRole('button', { name: 'Load more' }));
+	});
 
-    // it should not render the button if `canLoadMore` is false
-    it('should not render the the load more Button if `canLoadMore` is false', () => {
-        render(<PostCollection
-            headline="Whats new in Mumble...."
-            posts={loadedPosts}
-            canLoadMore={false}
-            canAdd={true}
-            autoUpdate={true}
-         />);
-        expect(screen.queryByRole('button', { name: 'Load more'}) ).toBeNull();
-    });
-    
-    // test if PostCollection component renders correctly if the user is logged in and allowed to add posts
-    it('should render the PostCreator component if `canAdd` is true with a send button', () => {
-        render(<PostCollection
-            headline="Whats new in Mumble...."
-            posts={loadedPosts}
-            canLoadMore={true}
-            canAdd={true}
-            autoUpdate={true}
-            />);
-        expect(screen.getByText('Deine Meinung zählt'));
-        expect(screen.getByRole('button', { name: 'Absenden' }) );
-    });
+	// it should not render the button if `canLoadMore` is false
+	it('should not render the the load more Button if `canLoadMore` is false', () => {
+		render(
+			<PostCollection
+				headline="Whats new in Mumble...."
+				posts={loadedPosts}
+				canLoadMore={false}
+				canAdd={true}
+				autoUpdate={true}
+			/>
+		);
+		expect(screen.queryByRole('button', { name: 'Load more' })).toBeNull();
+	});
 
-    // test if PostCollection component renders correctly if the user is not logged in and not allowed to add posts
-    it('should `not` render the PostCreator component if `canAdd` is false (user is not logged in)', () => {
-        render(<PostCollection
-            headline="Whats new in Mumble...."
-            posts={loadedPosts}
-            canLoadMore={true}
-            canAdd={false}
-            autoUpdate={true}
-            />);
-        expect(screen.queryByRole('button', { name: 'Absenden' }) ).toBeNull();
-    });
+	// test if PostCollection component renders correctly if the user is logged in and allowed to add posts
+	it('should render the PostCreator component if `canAdd` is true with a send button', () => {
+		render(
+			<PostCollection
+				headline="Whats new in Mumble...."
+				posts={loadedPosts}
+				canLoadMore={true}
+				canAdd={true}
+				autoUpdate={true}
+			/>
+		);
+		expect(screen.getByText('Deine Meinung zählt'));
+		expect(screen.getByRole('button', { name: 'Absenden' }));
+	});
 
-    // test if PostCollection component renders correctly if the feed returns no posts
-    it('should not render a message to the user if there are no Posts available', () => {
-        render(<PostCollection
-            headline="Whats new in Mumble...."
-            posts={[]}
-            canLoadMore={false}
-            canAdd={false}
-            autoUpdate={true}
-            />);
-        expect(screen.getByText('Keine Posts vorhanden.'));
-    });
+	// test if PostCollection component renders correctly if the user is not logged in and not allowed to add posts
+	it('should `not` render the PostCreator component if `canAdd` is false (user is not logged in)', () => {
+		render(
+			<PostCollection
+				headline="Whats new in Mumble...."
+				posts={loadedPosts}
+				canLoadMore={true}
+				canAdd={false}
+				autoUpdate={true}
+			/>
+		);
+		expect(screen.queryByRole('button', { name: 'Absenden' })).toBeNull();
+	});
 
-    //test if PostCollection component renders correctly if a filter is applied
-    it('should render the PostCollection component with a filter of userId', async () => {
-       render(<PostCollection
-                posts={loadedPosts}
-                canLoadMore={true}
-                canAdd={true}
-                autoUpdate={true}
-                filter={{creator: '201444056083988737'}}
-            />);
-        
-        const tomLinks = screen.getAllByRole('link', { name: 'tomschall' });
-        expect(tomLinks).toHaveLength(3);
-    });
+	// test if PostCollection component renders correctly if the feed returns no posts
+	it('should not render a message to the user if there are no Posts available', () => {
+		render(
+			<PostCollection
+				headline="Whats new in Mumble...."
+				posts={[]}
+				canLoadMore={false}
+				canAdd={false}
+				autoUpdate={true}
+			/>
+		);
+		expect(screen.getByText('Keine Posts vorhanden.'));
+	});
+
+	//test if PostCollection component renders correctly if a filter is applied
+	it('should render the PostCollection component with a filter of userId', async () => {
+		render(
+			<PostCollection
+				posts={loadedPosts}
+				canLoadMore={true}
+				canAdd={true}
+				autoUpdate={true}
+				filter={{ creator: '201444056083988737' }}
+			/>
+		);
+
+		const tomLinks = screen.getAllByRole('link', { name: 'tomschall' });
+		expect(tomLinks).toHaveLength(3);
+	});
 });
 
 describe('PostList Component input rendering', () => {
-    const loadedPosts = mockLoadedPosts as TPost[];
+	const loadedPosts = mockLoadedPosts as TPost[];
 
-    afterAll(cleanup);
-    // it should render the PostList component and display a post-hashtag as text
-    it('should render PostList component with text', () => {
-        render(<PostList 
-            posts={loadedPosts}
-            variant='timeline'
-            onAnswerPost={jest.fn()}
-            noPostsMessage='Keine Posts vorhanden.'
-            />);
-        expect(screen.getAllByText('#popcorn'));
-    });
+	afterAll(cleanup);
+	// it should render the PostList component and display a post-hashtag as text
+	it('should render PostList component with text', () => {
+		render(
+			<PostList
+				posts={loadedPosts}
+				variant="timeline"
+				onAnswerPost={jest.fn()}
+				noPostsMessage="Keine Posts vorhanden."
+			/>
+		);
+		expect(screen.getAllByText('#popcorn'));
+	});
 
-    //it should render also an empty postList
-    it('should render an empty PostList component with a feedback text if no Posts are loaded', () => {
-        render(<PostList 
-            posts={[]}
-            variant='timeline'
-            onAnswerPost={jest.fn()}
-            noPostsMessage='Keine Posts vorhanden.'
-            />);
-        expect(screen.getByText('Keine Posts vorhanden.'));
-    });
+	//it should render also an empty postList
+	it('should render an empty PostList component with a feedback text if no Posts are loaded', () => {
+		render(<PostList posts={[]} variant="timeline" onAnswerPost={jest.fn()} noPostsMessage="Keine Posts vorhanden." />);
+		expect(screen.getByText('Keine Posts vorhanden.'));
+	});
 });
