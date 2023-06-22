4bbb4ad42ab24de6e9f3d417ff5cec4f52953f24
Felix <felix.adam@tamedia.ch>
2023-06-22 21:28:41 +0200

    feat: configuration unit tests works

---
+++ jest.config.mjs
@@ -0,0 +1,36 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+import nextJest from 'next/jest.js';
+
+const babelConfigStyledComponents = {
+	presets: [['next/babel', { 'preset-react': { runtime: 'automatic' } }]],
+};
+
+const customJestConfig = {
+	verbose: true,
+	testEnvironment: 'jest-environment-jsdom',
+	moduleNameMapper: {
+		'@/(.*)$': '<rootDir>/src/$1',
+		'@smartive-education/pizza-hawaii': '<rootDir>/node_modules/@smartive-education/pizza-hawaii/dist/index.js',
+		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/src/__mocks__/fileMock.js',
+	},
+	/* Use babel-jest to transpile tests with the next/babel preset
+    https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+	transform: {
+		'^.+\\.(js|jsx|ts|tsx|mjs)$': ['babel-jest', babelConfigStyledComponents],
+	},
+	// match all test files with .test.(js|jsx|ts|tsx) extension in the __tests__ folder and in any subdirectory
+	// dont match .spec. files as they are used for integration tests (playwright)
+	testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
+	// An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+	testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/src/__mocks__/'],
+};
+
+const createJestConfig = nextJest({ dir: './' });
+
+const config = async () => ({
+	...(await createJestConfig(customJestConfig)()),
+	transformIgnorePatterns: ['node_modules/(?!(@smartive-education/pizza-hawaii)/)', '^.+\\.module\\.(css|sass|scss)$'],
+});
+
+export default config;
