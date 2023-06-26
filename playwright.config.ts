af4c3fac3665ad5a7babefd2c4599e733fc91359
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-06-26 22:06:41 +0200

    feat: add more tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const testBrowsers = ['Firefox']; // ['Firefox', 'Chrome', 'Safari', 'Mobile Chrome', 'Mobile Safari'];
+const testBrowsers = process.env.browsers || ['Firefox']; // ['Firefox', 'Chrome', 'Safari', 'Mobile Chrome', 'Mobile Safari'];
 
 export default defineConfig({
 	globalSetup: './tests/global.setup.ts',
