d872adf515319e01bce1ecee9f04b23e364dea88
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-06-26 22:26:24 +0200

    feat: update browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,8 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const testBrowsers = process.env.browsers || ['Firefox']; // ['Firefox', 'Chrome', 'Safari', 'Mobile Chrome', 'Mobile Safari'];
+const testBrowsers = process.env.browsers?.split("|") || ['Firefox']; // ['Firefox', 'Chrome', 'Safari', 'Mobile Chrome', 'Mobile Safari'];
+
 
 export default defineConfig({
 	globalSetup: './tests/global.setup.ts',
