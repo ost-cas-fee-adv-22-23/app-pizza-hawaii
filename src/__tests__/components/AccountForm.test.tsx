cc3c3522b1a0cf72cfad3e7d06dccbf27aaff4e3
Felix <felix.adam@tamedia.ch>
2023-06-29 17:29:38 +0200

    feat: better AccountForm tests

--- src/__tests__/components/AccountForm.test.tsx
+++ src/__tests__/components/AccountForm.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
-import { cleanup, fireEvent, render } from '@testing-library/react';
+import React from 'react';
+import { cleanup, fireEvent, getByText, render, screen } from '@testing-library/react';
 
 import { AccountForm, TAccountFormData } from '../../components/form/AccountForm';
 
@@ -41,29 +42,30 @@ const propsWrongUser = {
 
 describe('AccountForm with a provided user', () => {
 	afterEach(cleanup);
+	// test if the form returns the correct values when submitted
 	test('renders AccountForm component with first and last Name field with value', () => {
 		const { container } = render(<AccountForm {...propsUser} />);
 		expect(container.querySelector('input[name="firstName"]')).toHaveProperty('value', 'Felix');
 		expect(container.querySelector('input[name="lastName"]')).toHaveProperty('value', 'Adam');
 	});
 
-	test('renders AccountForm submitButton if all fields are filled', () => {
-		const { container } = render(<AccountForm {...propsUser} />);
-		expect(container.querySelector('button[type="submit"]')).toBeInTheDocument();
-	});
-
-	test('if matches snapshot of Form', () => {
-		const { container } = render(<AccountForm {...propsUser} />);
+	// test if the submit button is disabled when the form is loaded. expect the button container to have the class `opacity-50`
+	test('renders AccountForm component with disabled submit button when loading. The Visibility of Submitbutton is 50%', () => {
+		const { container } = render(<AccountForm {...propsUser} isLoading={true} />);
+		const buttonContainer = container.querySelector('.flex-1.opacity-50');
+		expect(buttonContainer).toHaveClass('opacity-50');
 		expect(container).toMatchSnapshot();
 	});
 
+	// test if the form subits empty strings when the form is empty
 	test('renders AccountForm with empty fields if no user Data is provided', () => {
 		const { container } = render(<AccountForm {...propsEmptyUser} />);
 		expect(container.querySelector('input[name="firstName"]')).toHaveProperty('value', '');
 		expect(container.querySelector('input[name="lastName"]')).toHaveProperty('value', '');
 	});
 
-	test('if two different passwords are entered, the input field text is red`', () => {
+	// test if the password maches the confirm password
+	test('if two different passwords are entered, the input field text is red', () => {
 		const { container } = render(<AccountForm {...propsWrongUser} />);
 		expect(container.querySelector('input[name="password"]')).toHaveProperty('value', 'strongPA$$W0RD');
 		expect(container.querySelector('input[name="confirmPassword"]')).toHaveProperty('value', 'strongPA$$--');
@@ -80,3 +82,4 @@ describe('AccountForm with a provided user', () => {
 		expect(firstNameInput).toHaveProperty('value', 'Martin');
 	});
 });
+
--- src/__tests__/components/postCollectionReducer.test.ts
+++ src/__tests__/components/postCollectionReducer.test.ts
@@ -5,17 +5,20 @@ import secondPostMock from '../../__mocks__/secondPost.json';
 jest.mock('next-auth/react');
 
 describe('the postCollectionReducer should', () => {
+    // test the initial state of the reducer
 	it('return initial state if new state is undefined', () => {
 		const newState = postCollectionReducer(undefined, {});
 		expect(newState).toEqual(initialState);
 	});
 
+    // test the LOADING action
 	it('should handle LOADING action', () => {
 		const action = { type: ActionType.LOADING, payload: true };
 		const newState = postCollectionReducer(initialState, action);
 		expect(newState.loading).toBe(true);
 	});
 
+    // test the POSTS_SET action with an array of posts
 	it('should handle POSTS_SET action', () => {
 		const posts = [firstPostMock, secondPostMock];
 		const action = { type: ActionType.POSTS_SET, payload: posts };
@@ -24,6 +27,7 @@ describe('the postCollectionReducer should', () => {
 		expect(newState.loading).toBe(false);
 	});
 
+    // test the POSTS_ADD action with antoher post
 	it('should handle POSTS_ADD action', () => {
 		const existingPosts = [firstPostMock];
 		const newPost = secondPostMock;
@@ -34,6 +38,7 @@ describe('the postCollectionReducer should', () => {
 		expect(newState.loading).toBe(false);
 	});
 
+    // test the POSTS_DELETE action with a given post id
 	it('should handle POSTS_DELETE action', () => {
 		const existingPosts = [firstPostMock, secondPostMock];
 		const postIdToDelete = '01GZFZES838XW8SV8QVWCJSN6C';
