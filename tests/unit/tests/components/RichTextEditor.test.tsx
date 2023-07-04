eac90defc801e6cba2ad7d4a8daa553916af059b
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-07-04 19:37:46 +0200

    feat: improve richtext Tests

--- tests/unit/tests/components/RichTextEditor.test.tsx
+++ tests/unit/tests/components/RichTextEditor.test.tsx
@@ -6,10 +6,13 @@ import { parse as parseRichText } from '../../../../src/utils/RichText';
 /**
  * Unit tests for the Richtext component.
  *
- * Basicly we want to know if all the different input types are rendered correctly.
+ * Basically we want to know if all the different input types are rendered correctly.
  * We test the following on Richtext
- * 1. Render Richtext component with text
- * 2. Render Richtext component with a link when a hastag is used in the text
+ * 1. Render component with text
+ * 2. Render component with correct font size class
+ * 3. Render component against snapshot
+ *
+ * We test the following on the parse function
  * 3. Render Richtext component with a link when a user is mentioned in the text
  * 4. Render Richtext component with a link when a url is used in the text
  * 5. Render Richtext component with the correct font size class
@@ -31,63 +34,31 @@ export const defaultSettings = {
 	mentionLinkPattern: '/user/$3',
 };
 
-const inputDataHashTag = 'hi there, I am a great fan of pizza hawaii. #pizza #hawaii';
-const inputDataLink = 'hi there, I want to share that https://example.com is a great website.';
-const inputDataUserMention = 'hi there, I want to share that @[Testuser|214652397815857409] is a crappy website.';
-const inputDataMarkdown = 'hi there, I want to share that [lookatthat](https://lookatthat.ch) is a simple website.';
-const inputDatawithBreaks = 'hi there, I want to share that long text with a line- \n, break and a pizza slize 🍕.';
+const examples = {
+	simple: 'hi there, I am a great fan of pizza hawaii.',
+	hashTag: 'hi there, I am a great fan of pizza hawaii. #pizza #hawaii',
+	link: 'hi there, I want to share that https://example.com is a great website.',
+	userMention: 'hi there, I want to share that @[Testuser|214652397815857409] is a crappy website.',
+	markdown: 'hi there, I want to share that [lookatthat](https://lookatthat.ch) is a simple website.',
+	withBreaks: 'hi there, I want to share that long text with a line- \n, break and a pizza slize 🍕.',
+};
 
 describe('Richtext Component input rendering', () => {
 	afterEach(cleanup);
-	// test if richtext component renders correctly
-	it('should render the Richtext component with text', () => {
-		render(
-			<Richtext size="M" as="div">
-				{inputDataHashTag}
-			</Richtext>
-		);
-		expect(screen.getByText('hi there, I am a great fan of pizza hawaii. #pizza #hawaii'));
+
+	it('should render the simple text', () => {
+		render(<Richtext size="M">{examples.simple}</Richtext>);
+		expect(screen.getByText(examples.simple));
 	});
 
-	// test if the Richtext renders correct html tags if `as` prop is specified
-	it('should render the Richtext component as `<article>` when specified', () => {
-		const { container } = render(
-			<Richtext size="M" as="article">
-				{inputDataLink}
-			</Richtext>
-		);
-		const articletag = container.querySelector('article');
-		expect(articletag?.tagName).toBe('ARTICLE');
-	});
-
-	// same test as above but with `section` instead of `article`
-	it('should render the Richtext component as `<section>` when specified', () => {
-		const { container } = render(
-			<Richtext size="M" as="section">
-				{inputDataLink}
-			</Richtext>
-		);
-		const sectiontag = container.querySelector('section');
-		expect(sectiontag?.tagName).toBe('SECTION');
-	});
-
-	// test if the Richtext component renders correct font specific font classes with `size` prop is specified
-	it('should render the Richtext component with `size` prop specified as `M` to have the correct css class', () => {
-		const { container } = render(
-			<Richtext size="M" as="div">
-				{inputDataLink}
-			</Richtext>
-		);
+	it('should render the correct style', () => {
+		const { container } = render(<Richtext size="M">{examples.simple}</Richtext>);
 		expect(container.getElementsByClassName('font-medium leading-normal text-l')).toHaveLength(1);
 	});
 
 	// test snapshot
 	it('should match the snapshot', () => {
-		const { container } = render(
-			<Richtext size="M" as="div">
-				{inputDataLink}
-			</Richtext>
-		);
+		const { container } = render(<Richtext size="M">{examples.link}</Richtext>);
 		expect(container).toMatchSnapshot();
 	});
 });
@@ -97,17 +68,17 @@ describe('test parseRichText functions', () => {
 	afterEach(cleanup);
 	// test plain text URLs to anchor tags creation when links are posted
 	it('should return link anchor tag when links are posted, clutched by `paragraph-tags`', () => {
-		const result = parseRichText(inputDataLink);
+		const result = parseRichText(examples.link);
 		expect(result).toBe(
-			`<p>${inputDataLink.replace('https://example.com', '<a href="https://example.com">https://example.com</a>')}</p>`
+			`<p>${examples.link.replace('https://example.com', '<a href="https://example.com">https://example.com</a>')}</p>`
 		);
 	});
 
 	// test link creation when hash tags are used
 	it('should return link creation when hash tags are used, clutched by `paragraph tags`', () => {
-		const result = parseRichText(inputDataHashTag);
+		const result = parseRichText(examples.hashTag);
 		expect(result).toBe(
-			`<p>${inputDataHashTag
+			`<p>${examples.hashTag
 				.replace('#pizza', '<a href="/tag/pizza">#pizza</a>')
 				.replace('#hawaii', '<a href="/tag/hawaii">#hawaii</a>')}</p>`
 		);
@@ -115,9 +86,9 @@ describe('test parseRichText functions', () => {
 
 	// test user mentions link creation
 	it('should return links to the corresponding user for all user mentions, clutched by `paragraph tags`', () => {
-		const result = parseRichText(inputDataUserMention);
+		const result = parseRichText(examples.userMention);
 		expect(result).toBe(
-			`<p>${inputDataUserMention.replace(
+			`<p>${examples.userMention.replace(
 				'@[Testuser|214652397815857409]',
 				'<a href="/user/214652397815857409">@Testuser</a>'
 			)}</p>`
@@ -126,9 +97,9 @@ describe('test parseRichText functions', () => {
 
 	// test markdown link creation
 	it('should return created links for all markdown links clutched by `paragraph tags`', () => {
-		const result = parseRichText(inputDataMarkdown);
+		const result = parseRichText(examples.markdown);
 		expect(result).toBe(
-			`<p>${inputDataMarkdown.replace(
+			`<p>${examples.markdown.replace(
 				'[lookatthat](https://lookatthat.ch)',
 				'<a href="https://lookatthat.ch">lookatthat</a>'
 			)}</p>`
@@ -137,7 +108,7 @@ describe('test parseRichText functions', () => {
 
 	// test line breaks creation renders correctly
 	it('should return replace line breaks with a `<br>`, clutched by `paragraph tags`', () => {
-		const result = parseRichText(inputDatawithBreaks);
-		expect(result).toBe(`<p>${inputDatawithBreaks.replace('\n', '<br>')}</p>`);
+		const result = parseRichText(examples.withBreaks);
+		expect(result).toBe(`<p>${examples.withBreaks.replace('\n', '<br>')}</p>`);
 	});
 });
